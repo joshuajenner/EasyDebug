0768632a44d60bbd7e8ec9a4c342b8affdc559b1
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2024-07-18 16:46:52 +0800

    Reworked PrintValue to ValueLabel

--- addons/easydebug/easy_debug.gd
+++ addons/easydebug/easy_debug.gd
@@ -1,22 +1,56 @@
 extends CanvasLayer
 
-@onready var print_container: VBoxContainer = $Margin/PanelContainer/MarginContainer/VBoxContainer
 
-var print_value = preload("res://addons/easydebug/overlay/print_value.tscn")
+@onready var _keys_container: VBoxContainer = %Keys
+@onready var _values_container: VBoxContainer = %Values
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.print("Test", Vector2.ZERO)
-	self.print("Test2", false)
+var _value_label: PackedScene = preload("res://addons/easydebug/overlay/print_value.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _ready() -> void:
+	self.print("speed_movement", Vector3.LEFT)
+	self.print("speed_projectile", Vector3.ZERO)
+	self.print("is_player_grounded", true)
+
+
+func _physics_process(delta: float) -> void:
+	self.print("Time", Time.get_ticks_msec())
+	
 
 
 func print(key: String, value: Variant) -> void:
-	# TODO - Should check if key already exists
-	var new_print = print_value.instantiate() as PrintValue
-	print_container.add_child(new_print)
-	new_print.init_display(key, value)
+	for key_label: Label in _keys_container.get_children():
+		if key == key_label.text.trim_suffix(":"):
+			var index: int = key_label.get_index()
+			var matched_value: ValueLabel = _values_container.get_child(index)
+			matched_value.update_value(value)
+			return
+	
+	var new_key_label = Label.new()
+	_keys_container.add_child(new_key_label)
+	new_key_label.text = key + ":"
+	
+	var new_value_label = _value_label.instantiate() as ValueLabel
+	_values_container.add_child(new_value_label)
+	new_value_label.update_value(value)
+
+
+func remove(key: String) -> void:
+	for key_label: Label in _keys_container.get_children():
+		if key == key_label.text.trim_suffix(":"):
+			var index: int = key_label.get_index()
+			key_label.queue_free()
+			_values_container.get_child(index).queue_free()
+
+
+func clear() -> void:
+	for child: Node in _keys_container.get_children():
+		child.queue_free()
+		
+	for child: Node in _values_container.get_children():
+		child.queue_free()
+
+
+func _on_button_pressed() -> void:
+	clear()
--- addons/easydebug/overlay/print_value.gd
+++ addons/easydebug/overlay/print_value.gd
@@ -1,28 +1,19 @@
-class_name PrintValue
-extends MarginContainer
-
-
-@onready var key: Label = $HBoxContainer/Key
-@onready var value: RichTextLabel = $HBoxContainer/Value
-
-
-func init_display(new_key: String, new_value: Variant) -> void:
-	key.text = new_key
-	value.text = get_string_from_value(new_value)
+class_name ValueLabel
+extends RichTextLabel
 
 
 func update_value(new_value: Variant) -> void:
-	value.text = get_string_from_value(new_value)
+	text = _get_string_from_value(new_value)
 
 
-func get_string_from_value(new_value: Variant) -> String:
+func _get_string_from_value(new_value: Variant) -> String:
 	if typeof(new_value) == TYPE_BOOL:
-		return get_coloured_string_from_bool(new_value)
+		return _get_coloured_string_from_bool(new_value)
 	else:
 		return "[right][color=\"Deepskyblue\"]" + str(new_value) + "[/color][/right]"
 
 
-func get_coloured_string_from_bool(value: bool) -> String:
+func _get_coloured_string_from_bool(value: bool) -> String:
 	if value:
 		return "[right][color=\"green\"]true[/color][/right]"
 	else:
